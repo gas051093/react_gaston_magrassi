9116b048815549fd3daf165748c08d1ffaf3a120
gaston-magrassi <gas051093@hotmail.com.ar>
2025-07-05 00:12:18 -0300

    se agrgeo un swal para confirmar la eliminacion del carrito

--- src/components/CartList/CartList.jsx
+++ src/components/CartList/CartList.jsx
@@ -5,10 +5,31 @@ import { formatPrice } from '../../Helpers/priceFormat'
 import { useContext } from "react"
 import { useNavigate } from "react-router"
 import EmptyCart from "../EmptyCart"
+import Swal from "sweetalert2"
 function CartList({ cart, cartTotal}) {
   const { clearCart } = useContext(CartContext)
   const navigate = useNavigate();
-
+  const showSwal = () => {
+        Swal.fire({
+          title: "Desea vaciar el carrito?",
+          text: "Esto eliminara el carrito completo",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Si, eliminar",
+        })
+        .then((result) => {
+          if (result.isConfirmed) {
+            clearCart()
+            Swal.fire({
+              title: "Eliminado!",
+              text: "Su carrito esta vacio!",
+              icon: "success",
+            });
+          }
+        });
+    };
     return (
       <>
         <div className="CartList">
@@ -24,7 +45,7 @@ function CartList({ cart, cartTotal}) {
                 <p className="p-0 m-0 mb-3 text-end CartList__total">Total: {formatPrice(cartTotal())}</p>
                 <div className="d-flex justify-content-center gap-3">
                   <button className="CartList__btn" onClick={() => navigate('/checkout')}>Comprar</button>
-                  <button className="CartList__btn " onClick={ () => clearCart()}>Vaciar carrito</button>
+                  <button className="CartList__btn " onClick={ () => showSwal()}>Vaciar carrito</button>
                 </div>
               </div>
             )}
